3b01f2f1264e4e257eb8cf35f5cf234f19c2e774
nanaDio <zhenzhong.fu@gmail.com>
2023-02-22 15:03:30 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
-from http.server import BaseHTTPRequestHandler
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from datetime import datetime
 
 import os
-import urllib.request
+import urllib
 import json
 import random
 import urlparse
+import io,shutil
+import cgi
 
 finnlp_file = "../2synonyms.json"
 
@@ -19,7 +21,7 @@ def toHuman(content, percentToChange, collection, ignore_quotes):
         f = open(finnlp_file)
         synonyms = json.load(f)
         print("Loaded file ")
-        
+
         words = content.split(" ")
         num_words = int(len(words) * percentToChange / 100)
         chosen_indices = random.sample(range(len(words)), num_words)
@@ -39,7 +41,7 @@ def toHuman(content, percentToChange, collection, ignore_quotes):
                 if len(word) > 3 and word in synonyms.keys() and len(synonyms[word]) != 0:
                     word = random.choice(synonyms[word])
 
-                    newWords = "{}{}{}".format(newWords, word, endswith)
+                newWords = "{}{}{}".format(newWords, word, endswith)
                 if i != len(words) - 1:
                     newWords = "{}{}".format(newWords, " ")
             else:
@@ -52,17 +54,39 @@ def toHuman(content, percentToChange, collection, ignore_quotes):
         print(e)
         exit()
 
-content ="John F. Kennedy, who served as the 35th President of the United States from 1961 until his assassination in 1963, is widely considered as one of the best Presidents in American history. He was a charismatic leader who inspired and united the nation, particularly during the Civil Rights Movement. Kennedy's speeches, such as his inaugural address in which he famously said Ask not what your country can do for you, ask what you can do for your country, and his famous Moon speech in which he challenged the nation to put a man on the moon within a decade, are still remembered and quoted today. Additionally, Kennedy's foreign policy was marked by a strong commitment to peace and diplomacy, as exemplified by the Cuban Missile Crisis, where his cool and steady leadership helped to defuse a potentially catastrophic conflict with the Soviet Union. Overall, JFK's legacy as a visionary leader who inspired a generation and left a lasting impact on the world continues to make him one of the most beloved Presidents in American history."
-result = toHuman(content, 20, 2, True)
-print(content)
-print(result)
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+
         parsed_path = urlparse.urlparse(self.path)
-        data=parsed_path.query[1]
+        print(self.path)
+        #data=parsed_path.query[0]
+
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
         return
+
+    def do_POST(self):
+        form = cgi.FieldStorage(
+        fp=self.rfile,
+        headers=self.headers,
+        environ={'REQUEST_METHOD':'POST',
+        'CONTENT_TYPE':self.headers['Content-Type'],
+        })
+
+        content = ""
+        result = ""
+        for field in form.keys():
+            if field == "text":
+                content = form[field].value
+                result = toHuman(content, 60, 2, False)
+                break
+
+        enc = "UTF-8"
+        self.send_response(200)
+        self.send_header("Content-type", "application/json; charset=%s" % enc)
+        self.end_headers()
+        #self.wfile.write('Client: %s\n' % str(self.client_address))
+        #self.wfile.write('Path: %s\n' % self.path)
+        self.wfile.write("{\"text\":\""+content+"\"}")
